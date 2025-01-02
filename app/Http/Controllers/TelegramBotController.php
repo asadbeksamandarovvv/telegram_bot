9cd11a025ee6d63a83033531bd9dfad1380c2988
ddev <doston.3401@gmail.com>
2025-01-02 17:17:54 +0500

    ishlab duribdi

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramBotController extends Controller
 {
-    //
+    public function handle(Request $request)
+    {
+        $telegram = Telegram::getWebhookInfo();
+
+        Log::info('update', [$telegram] );
+    }
 }
