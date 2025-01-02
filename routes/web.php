8baea8084f198b26f50d398ee4370c2ed94ef945
ddev <doston.3401@gmail.com>
2025-01-02 16:27:53 +0500

    added webhook and ngrok

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('setwebhook', function () {
+    $response = Telegram::setWebhook(['url' => 'https://3872-213-230-93-97.ngrok-free.app/api/telegram/webhook']);
+
 });
